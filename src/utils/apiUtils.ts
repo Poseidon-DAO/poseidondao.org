cc30dfde3c3595406ef2be72aefc17cf2e44c8db
Mateo <mateorubinstein@icloud.com>
2022-09-14 19:20:22 +0100

    Remove console log

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,5 +1,4 @@
 export const checkError = async (response: any) => {
-  console.log("Response", response);
   if (!(response.status >= 200 && response.status < 400)) {
     const { message } = await toJson(response);
     const error = new Error(message);
