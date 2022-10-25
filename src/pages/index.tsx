483b87f6c8eee4eef002f865ab0e98e351df5ea2
viganzeqiri <viganzeqirii@gmail.com>
2022-10-25 17:49:47 +0200

    added next-seo package and called the component on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { Box } from "@chakra-ui/react";
+import { NextSeo } from "next-seo";
+
 import {
   PoseidonDAO,
   Collections,
@@ -20,6 +22,10 @@ const Home: NextPage = () => {
           "linear-gradient(180deg,hsl(238deg 39% 20%) 0%,hsl(239deg 38% 14%) 23%,hsl(288deg 45% 2%) 100%)",
       }}
     >
+      <NextSeo
+        title="Poseido DAO"
+        description="Making Crypt Art the 21th Century Art"
+      />
       <Hero />
       <CollectionStats />
       <Collections />
