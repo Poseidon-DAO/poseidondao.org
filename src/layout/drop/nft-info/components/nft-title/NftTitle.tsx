9b38f4cde613a81a2d76b30d494ed9b470564fb5
viganzeqiri <viganzeqirii@gmail.com>
2022-11-16 07:09:01 +0100

    removed unused JS

--- src/layout/drop/nft-info/components/nft-title/NftTitle.tsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-const NftTitle = ({ title }: { title: string }) => {
-  return <Heading fontSize={{ sm: "6xl", lg: "4xl" }}>{title}</Heading>;
-};
-
-export { NftTitle };
