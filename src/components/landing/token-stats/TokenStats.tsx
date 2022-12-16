5207b20e054ce1674addf5899315ff58de9204c5
viganzeqiri <viganzeqirii@gmail.com>
2022-12-17 00:48:19 +0100

    added ui for sections

--- src/components/landing/token-stats/TokenStats.tsx
+++ src/components/landing/token-stats/TokenStats.tsx
@@ -1,37 +1,82 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import { Container } from "components/container";
-import { useAccount } from "wagmi";
 
 const TokenStats = () => {
   return (
     <Box py={20} w="100vw">
       <Container>
-        <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-          <GridItem
-            w="100%"
-            h="35vh"
-            border="1px solid"
-            borderColor="brand.red"
-          />
-          <GridItem
-            w="100%"
-            h="35vh"
-            border="1px solid"
-            borderColor="brand.red"
-          />
-          <GridItem
-            w="100%"
-            h="35vh"
-            border="1px solid"
-            borderColor="brand.red"
-          />
-          <GridItem
-            w="100%"
-            h="35vh"
-            border="1px solid"
-            borderColor="brand.red"
-          />
-        </Grid>
+        <Flex flexWrap="wrap" flexDir="column" gap={6}>
+          <Flex gap={6} flexDir={{ sm: "column", lg: "row" }}>
+            <Box
+              w={{ sm: "100%", lg: "50%" }}
+              p={8}
+              h="35vh"
+              border="1px solid"
+              borderColor="brand.red"
+              pos="relative"
+            >
+              <Text fontSize="3xl">Total PDN</Text>
+              <Text fontSize="5xl" fontWeight="bold">
+                200,000.00 PDN
+              </Text>
+
+              <Box pos="absolute" bottom={8}>
+                <Button size="xl" w="160px">
+                  BURN
+                </Button>
+              </Box>
+            </Box>
+
+            <Box
+              w={{ sm: "100%", lg: "50%" }}
+              p={8}
+              h="35vh"
+              border="1px solid"
+              borderColor="brand.red"
+            >
+              <Text fontSize="3xl">Total Vested PDN</Text>
+              <Text fontSize="5xl" fontWeight="bold">
+                100,000,000.00 PDN
+              </Text>
+            </Box>
+          </Flex>
+
+          <Flex gap={6} flexDir={{ sm: "column", lg: "row" }}>
+            <Box
+              w={{ sm: "100%", lg: "50%" }}
+              p={8}
+              h="35vh"
+              border="1px solid"
+              borderColor="brand.red"
+              pos="relative"
+            >
+              <Text fontSize="3xl">Available PDN</Text>
+              <Text fontSize="5xl" fontWeight="bold">
+                10,000.00 PDN
+              </Text>
+
+              <Box pos="absolute" bottom={8}>
+                <Button size="xl" w="160px">
+                  CLAIM
+                </Button>
+              </Box>
+            </Box>
+
+            <Box
+              w={{ sm: "100%", lg: "50%" }}
+              p={8}
+              h="35vh"
+              border="1px solid"
+              borderColor="brand.red"
+              pos="relative"
+            >
+              <Text fontSize="3xl">Next PDN available in</Text>
+              <Text fontSize="5xl" fontWeight="bold">
+                32 days
+              </Text>
+            </Box>
+          </Flex>
+        </Flex>
       </Container>
     </Box>
   );
